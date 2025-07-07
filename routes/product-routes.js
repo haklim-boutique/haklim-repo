32038ea6a97e1e57832f9086f75dfd6f4197d96f
haklim-boutique <haklimboutique@gmail.com>
2025-07-08 02:47:40 +0300

    Add files via upload

---
+++ routes/product-routes.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+
+const  { fetchProducts }  = require('../controllers/products-controller');
+
+// Sample products data
+const products = [
+    {
+        id: 1,
+        name: "The Milano Handbag",
+        price: 395,
+        oldPrice: 450,
+        image: "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=736&q=80",
+        badge: "New"
+    },
+    {
+        id: 2,
+        name: "Florentine Silk Scarf",
+        price: 185,
+        image: "https://images.unsplash.com/photo-1542272604-787c3835535d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1026&q=80",
+        badge: "Bestseller"
+    },
+    {
+        id: 3,
+        name: "Venezia Leather Wallet",
+        price: 275,
+        image: "https://images.unsplash.com/photo-1592878904946-b3cd8ae243d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80"
+    },
+    {
+        id: 4,
+        name: "Cashmere Crewneck",
+        price: 320,
+        image: "https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
+        badge: "Limited"
+    },
+    {
+        id: 5,
+        name: "Silk Evening Gown",
+        price: 550,
+        oldPrice: 650,
+        image: "https://images.unsplash.com/photo-1539109136881-3be0616acf4b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        badge: "New"
+    },
+    {
+        id: 6,
+        name: "Leather Crossbody Bag",
+        price: 280,
+        image: "https://images.unsplash.com/photo-1594035910387-fea47794261f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80"
+    },
+    {
+        id: 7,
+        name: "Wool Cashmere Coat",
+        price: 420,
+        oldPrice: 500,
+        image: "https://images.unsplash.com/photo-1551232864-3f0890e580d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80",
+        badge: "Sale"
+    },
+    {
+        id: 8,
+        name: "Linen Summer Dress",
+        price: 195,
+        image: "https://images.unsplash.com/photo-1585487000160-6ebcfceb0d03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=734&q=80"
+    },
+    {
+        id: 9,
+        name: "Velvet Blazer",
+        price: 380,
+        image: "https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1179&q=80"
+    },
+    {
+        id: 10,
+        name: "Silk Pajama Set",
+        price: 220,
+        image: "https://images.unsplash.com/photo-1596755094514-f87e34085b2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"
+    }
+];
+
+// Endpoint to get products
+router.get('/get-products', fetchProducts);
+
+
+module.exports = router;
