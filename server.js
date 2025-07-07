c429b9f364bed2bea24375f9b87a745d75b81b1a
haklim-boutique <haklimboutique@gmail.com>
2025-07-08 02:46:03 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const productRoutes = require('./routes/product-routes');
+const PORT = 4000;
+
+
+
+// Endpoint to get products
+app.use('/fetch', productRoutes);
+
+
+// Serve index.html for the main site
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Serve admin.html for the admin panel
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
