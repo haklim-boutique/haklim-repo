1ce9e35faedd6872815e61ab1dd642ff148e69d0
haklim-boutique <haklimboutique@gmail.com>
2025-07-10 14:31:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const productRoutes = require('./routes/product-routes');
+const crudRoutes = require('./routes/crud-routes');
+const checkoutRoutes = require('./routes/checkout-routes');
 const PORT = 4000;
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+// For JSON:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 // Endpoint to get products
 app.use('/fetch', productRoutes);
+app.use('/crud', crudRoutes);
+app.use('/checkout', checkoutRoutes);
+
 
 app.get('/awake', (req, res) => {
   res.send('System Awoken');
