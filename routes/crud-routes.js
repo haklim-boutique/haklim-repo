0d7958075c7fc11bdf694a75dad356fb414f72d8
haklim-boutique <haklimboutique@gmail.com>
2025-07-23 16:26:41 -0700

    Update crud-routes.js

--- routes/crud-routes.js
+++ routes/crud-routes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const  { postProduct, editProduct, deleteProduct, fetchProductsAdmin }  = require('../controllers/products-controller');
 const { fetchOrders } = require('../controllers/orders-controller');
+const crudOperations = require('../controllers/admin-controller');
 // Endpoint to get products
 router.post('/add-product', postProduct);
 router.get('/edit-product', editProduct);
 router.delete('/delete-product/:productId', deleteProduct);
-
+router.post('/delete-product', crudOperations.deleteProduct);
+router.post('/update-order-status', crudOperations.updateOrderStatus);
+router.post('/update-product-status', crudOperations.updateProductStatus);
 
 // Get all products
 router.get('/fetch-products', fetchProductsAdmin);
