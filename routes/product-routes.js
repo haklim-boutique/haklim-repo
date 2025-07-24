10d93594f1575fab3c7087e91632852c9dab21c1
haklim-boutique <haklimboutique@gmail.com>
2025-07-24 04:19:57 -0700

    Update product-routes.js

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -2,9 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 const  { fetchProducts }  = require('../controllers/products-controller');
-
+const { 
+    fetchData, 
+    updateOrderStatus, 
+    cancelOrder, 
+    updateProductStatus, 
+    addProduct, 
+    deleteProduct, 
+    updateProduct
+ } = require('../controllers/admin-controller');
 // Endpoint to get products
 router.get('/get-products', fetchProducts);
+router.get('/get-admin-data', fetchData);
+
+
+router.post('/cancel-order', cancelOrder);
+router.post('/add-product', addProduct);
+router.post('/delete-product', deleteProduct);
+router.put('/update-product', updateProduct);
 
 
 module.exports = router;
